abc46933d08942d7ed81debbebafa17e858b5aac
Liam Gibbs <lgibbs@expediagroup.com>
2026-01-04 15:23:51 +0000

    Add queue polling + command processing (merging from fpl-worker repo)

--- notifiers/new_trade_notifier.py
+++ notifiers/new_trade_notifier.py
@@ -1,28 +1,29 @@
 from api.fpl_api import FplAPI
-from api.notify_api import NotifyApi
+from api.queue_api import QueueApi
 from repo.state import State
 import logging
 
 logger = logging.getLogger(__name__)
 
 class NewTradeNotifier(object):
-    def __init__(self, fpl_api: FplAPI, notify_api: NotifyApi, league_id: int, state: State):
+    def __init__(self, fpl_api: FplAPI, queue_api: QueueApi, league_id: int, state: State):
         self.fpl_api = fpl_api
-        self.notify_api = notify_api
+        self.queue_api = queue_api
         self.league_id = league_id
         self.state = state
 
     def check_and_notify_new_trade(self):
         transactions = self.fpl_api.get_transactions(self.league_id)
-        players = self.fpl_api.get_players()
         teams = self.fpl_api.get_teams(self.league_id)
-
+        players = self.fpl_api.get_players()
         # Sort by most recent first
         sorted_tx = sorted(transactions, key=lambda x: x['response_time'], reverse=True)
 
         for tx in sorted_tx:
             trade_id = tx["id"]
             if trade_id != self.state.last_trade_id:
+
+
                 # Build trade message
                 offered_team = teams[tx['offered_entry']]
                 received_team = teams[tx['received_entry']]
@@ -30,7 +31,6 @@ class NewTradeNotifier(object):
                 outs = "\n".join([players[trade_item['element_out']] for trade_item in tx['tradeitem_set']])
                 message = f"🚨 New Trade Alert 🚨\n*{offered_team}*\n{ins}\n ↔️ \n*{received_team}*\n{outs}"
                 logger.info(f"New trade detected, id: {trade_id}")
-                logger.info(message)
-                self.notify_api.notify(message)
+                self.queue_api.notify(message)
                 self.state.save_last_trade_id(trade_id)
             break  # Only alert on newest trade
