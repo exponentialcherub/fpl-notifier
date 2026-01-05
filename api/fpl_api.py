b3b706cc10a558e32f2e33b9756e4af534637ea3
Liam Gibbs <lgibbs@expediagroup.com>
2026-01-05 17:42:19 +0000

    Team name fix

--- api/fpl_api.py
+++ api/fpl_api.py
@@ -26,7 +26,7 @@ class FplAPI(object):
 
     def get_teams(self, league_id):
         league_details = self._get_details(league_id)
-        teams = {team['entry_id']: team['entry_name'] for team in league_details['league_entries']}
+        teams = {team['id']: team['entry_name'] for team in league_details['league_entries']}
         return teams
 
     def get_standings(self, league_id):
